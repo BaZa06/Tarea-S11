55b98d21fd5f7da5781e535d0b78898a88bf1d66
BaZa06 <bazeledon@uamv.edu.ni>
2025-06-09 12:56:07 -0600

    Se realizo una modificación

--- ejercicio2.py
+++ ejercicio2.py
@@ -3,9 +3,19 @@
 # Por ejemplo, si se ingresa 10, la función debe devolver "1010". No uses funciones integradas de conversión a binario; 
 # implementa la lógica dividiendo el número y construyendo la cadena manualmente.
 
+def numero_a_binario(numero):
+    if numero < 0:
+        return "El numero debe ser postivo"
+    elif numero == 0:
+        return "0"
+    binario = ""
+    while numero > 0:
+        residuo = numero % 2
+        binario = str(residuo) + binario
+        numero = numero // 2
+    return binario
 
-numero = int(input("Ingrese un número entero positivo: "))
+numero_binario = int(input("Ingrese un número entero positivo: "))
+numero_base = numero_a_binario(numero_binario)
 
-
-
-
+print(f"El resultado de convertir el número {numero_binario} a binario es: {numero_base}")
