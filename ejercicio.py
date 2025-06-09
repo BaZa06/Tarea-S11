fde30613fdb34c70b56d175adb4fa0c06a331a3d
BaZa06 <bazeledon@uamv.edu.ni>
2025-06-09 11:47:38 -0600

    Se realizo una modificación

--- ejercicio.py
+++ ejercicio.py
@@ -3,15 +3,23 @@
 # El factorial de un número n es el producto de todos los enteros positivos desde 1 hasta n (por ejemplo, 5! = 5 * 4 * 3 2   1 = 120).
 # Asegúrate de manejar el caso especial donde n = 0, que devuelve 1.
 
-numero = int(input("Ingrese un número entero positivo: "))
 
-if numero < 0:
-    print("El numero debe de ser positivo y entero")
-else:
-    def factorial_numero(n):
-        if n == 0:
-            return 1
+
+def factorial_numero(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial_numero(n - 1)
+    
+while True:
+    numero = int(input("Ingrese un número entero positivo: "))
+    try:
+        numero_válido = int(numero)
+        if numero_válido < 0:
+            print("El numero debe de ser positivo y entero")
         else:
-            return n * factorial_numero(n - 1)
-    print(f"El facorial de {numero} es: {factorial_numero(numero)}")
-        
+            break
+    except ValueError:
+        print("Por favor, ingres un número entero positivo:")
+    
+print(f"El factorial del {numero} es: {factorial_numero(numero)}")
--- ejercicio2.py
+++ ejercicio2.py
@@ -2,3 +2,10 @@
 #Planteamiento: Escribe una función que reciba un número entero positivo y devuelva una cadena con su representación en binario (base 2).
 # Por ejemplo, si se ingresa 10, la función debe devolver "1010". No uses funciones integradas de conversión a binario; 
 # implementa la lógica dividiendo el número y construyendo la cadena manualmente.
+
+
+numero = int(input("Ingrese un número entero positivo: "))
+
+
+
+
